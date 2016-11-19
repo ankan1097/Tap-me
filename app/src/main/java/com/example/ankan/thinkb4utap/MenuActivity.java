569ad7e31f0df096b1ec6dbe6f7a7f570f038d35
Ankan Sardar <iankan@cse.iitb.ac.in>
2016-11-20 00:41:18 +0530

    first

---
+++ app/src/main/java/com/example/ankan/thinkb4utap/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.ankan.thinkb4utap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
