59904798fc8acb058510c6a9ec9a822016325ef0
Raunaque97 <rpatra1997@gmail.com>
2016-11-20 05:29:14 +0530

    add function to generate random Qs from a set

--- app/src/main/java/com/example/ankan/thinkb4utap/GameActivity.java
+++ app/src/main/java/com/example/ankan/thinkb4utap/GameActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -12,11 +13,13 @@ public class GameActivity extends AppCompatActivity {
     ProgressBar mProgressBar;
     CountDownTimer mCountDownTimer;
     int i=0;
+    TextView qtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        qtext=(TextView)findViewById(R.id.questiontext);
 
         mProgressBar=(ProgressBar)findViewById(R.id.progressbar);
         mProgressBar.setProgress(i);
@@ -35,10 +38,38 @@ public class GameActivity extends AppCompatActivity {
                 //Do what you want
                 i++;
                 mProgressBar.setProgress(i);
+                if(i==100)
+                    i=0;
             }
+
         };
         mCountDownTimer.start();
+        getquestion((int)(Math.random()*4));
     }
 
+    public void getquestion(int Qch) {
+        String[] Q0 = {"wait", "just wait", "do nothing", "It wont be long"};
+        String[] Q1 = {"tap 1", "just tap 1", "donot tap 2 tap 1", "don't tap 3 tap 1"};
+        String[] Q2 = {"tap 2", "just tap 2", "donot tap 1 tap 2", "don't tap 3 tap 2"};
+        String[] Q3 = {"tap 3", "just tap 3", "donot tap 1 tap 3", "don't tap 2 tap 3"};
 
+        int ch;
+        String qesn;
+        switch (Qch)
+        {
+            case 0:
+                qesn = Q0[(int) (Math.random() * Q0.length)];
+                break;
+            case 1:
+                qesn = Q1[(int) (Math.random() * Q1.length)];
+                break;
+            case 2:
+                qesn = Q2[(int) (Math.random() * Q2.length)];
+                break;
+            default:
+                qesn = Q3[(int) (Math.random() * Q3.length)];
+                break;
+        }
+        qtext.setText(qesn);
+    }
 }
