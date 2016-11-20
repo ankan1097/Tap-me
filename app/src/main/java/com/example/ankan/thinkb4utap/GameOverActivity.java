cbb421b764f23411e99dccf94bc4612c9cd715b4
Ankan Sardar <iankan@cse.iitb.ac.in>
2016-11-21 04:53:33 +0530

    gali snd effect added

--- app/src/main/java/com/example/ankan/thinkb4utap/GameOverActivity.java
+++ app/src/main/java/com/example/ankan/thinkb4utap/GameOverActivity.java
@@ -1,6 +1,7 @@
 package com.example.ankan.thinkb4utap;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,8 @@ public class GameOverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.gali);
+        mp.start();
         tv = (TextView) findViewById(R.id.gameovertext);
         tv.setText("game over loser");
         gob = (Button) findViewById(R.id.playagain);
--- app/src/main/java/com/example/ankan/thinkb4utap/MenuActivity.java
+++ app/src/main/java/com/example/ankan/thinkb4utap/MenuActivity.java
@@ -1,6 +1,8 @@
 package com.example.ankan.thinkb4utap;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,8 +35,31 @@ public class MenuActivity extends AppCompatActivity {
 
             }
         });
-
-
-
     }
+
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Exit THINK B4U TAP?");
+        alertDialogBuilder
+                .setMessage("Click yes if u had enough!")
+                .setCancelable(false)
+                .setPositiveButton("Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                moveTaskToBack(true);
+                                android.os.Process.killProcess(android.os.Process.myPid());
+                                System.exit(1);
+                            }
+                        })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+
 }
