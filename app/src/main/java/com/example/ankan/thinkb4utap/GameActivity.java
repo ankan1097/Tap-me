ccd1f7847a878180807feeff41b132b77d199fb7
Raunaque97 <rpatra1997@gmail.com>
2016-11-21 06:06:30 +0530

    more Qs added

--- app/src/main/java/com/example/ankan/thinkb4utap/GameActivity.java
+++ app/src/main/java/com/example/ankan/thinkb4utap/GameActivity.java
@@ -48,8 +48,9 @@ public class GameActivity extends AppCompatActivity {
                 if (Integer.parseInt(bt1.getText().toString()) == ans) {
                     ans=(int)(Math.random()*4);
                     getquestion(ans);
-                    setButtntext();
-                    score++;
+                    if( ((int)(Math.random() * 10)) <=2 ) {//30% chance
+                        setButtntext();
+                    }                    score++;
                     scr.setText(""+score);
                     i=0;
                     mCountDownTimer.cancel();
@@ -70,8 +71,9 @@ public class GameActivity extends AppCompatActivity {
 
                     ans = (int) (Math.random() * 4);
                     getquestion(ans);
-                    setButtntext();
-                    score++;
+                    if( ((int)(Math.random() * 10)) <=2 ) {//30% chance
+                        setButtntext();
+                    }                    score++;
                     scr.setText(""+score);
                     i=0;
                     mCountDownTimer.cancel();
@@ -92,7 +94,9 @@ public class GameActivity extends AppCompatActivity {
                 if ( Integer.parseInt(bt3.getText().toString()) == ans ){
                     ans=(int)(Math.random()*4);
                     getquestion(ans);
-                    setButtntext();
+                    if( ((int)(Math.random() * 10)) <=2 ) {//30% chance
+                        setButtntext();
+                    }
                     score++;
                     scr.setText(""+score);
                     i=0;
@@ -121,10 +125,36 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void getquestion(int Qch) {
-        String[] Q0 = {"wait", "just wait", "do nothing", "It wont be long"};
-        String[] Q1 = {"tap 1", "just tap 1", "do not tap 2 tap 1", "don't tap 3 tap 1"};
-        String[] Q2 = {"tap 2", "just tap 2", "do not tap 1 tap 2", "don't tap 3 tap 2"};
-        String[] Q3 = {"tap 3", "just tap 3", "do not tap 1 tap 3", "don't tap 2 tap 3"};
+        String[] Q0 = { "wait","W-A-I-T","sit for 2 seconds", "just wait", "do nothing", "It wont be long",
+                "dont tap 1 or 2 or 3","next one is :","next one is comming just wait",
+                "tap nothing","T-A-P nothing","TAP 2 for defeat","TAP 1 if You want to end this",
+                "TAP 2 if You want to end this","TAP 3 or 1 if You accept defeat","TAP 3 to sleep",};
+
+        String[] Q1 = {"tap 1", "just tap 1", "do not tap 2 tap 1", "don't tap 3 tap 1",
+                "tap 1 to go to next one", "just tap 1 to go to next one", "do not tap 2 tap 1 to go to next one",
+                "don't tap 3 or 2 tap 1 to go to next one","What is 2-1 Tap that one","What is 3-2 Tap that one",
+                "tap 1 and wait", "just tap 1 and wait", "do not TAP2 TAP1",
+                "don't T-A-P 3 T-A-P 1","sqrt(3249)/19 = ? don't calculate just tap 1",
+                "Answer of (221/17-11)is?  and tap 1 for next","TAP 1 if You want to go to next level",
+                "TAP 1 if You want to increase score"};
+
+        String[] Q2 = {"tap 2", "just tap 2", "do not tap 3 tap 2", "don't tap 1 tap 2",
+                "tap 2 to go to next one", "just tap 2 to go to next one", "do not tap 3 tap 1 to go to next one",
+                "don't tap 3 or 1 tap 2 to go to next one","What is 3-1 Tap that one","What is 1+1 Tap that one",
+                "tap 2 and wait", "just tap 2 and wait", "do not TAP3 TAP2",
+                "don't T-A-P 3 T-A-P 2","sqrt(5249)/19 = ? don't calculate just tap 2",
+                "Answer of (251/17-11)is?  and tap 2 for next","TAP 2 if You want to go to next level",
+                "TAP 2 if You want to increase score"};
+
+        String[] Q3 = {"tap 3", "just tap 3", "do not tap 2 tap 3", "don't tap 1 tap 3",
+                "tap 3 to go to next one", "just tap 3 to go to next level", "do not tap 2 tap 3 to go to next one",
+                "don't tap 1 or 2 tap 3 to go to next one","What is 2+1 Tap that one","What is 3-0 Tap that one",
+                "tap 3 and wait", "just tap 3 and wait", "do not TAP2 TAP3",
+                "don't T-A-P 1 T-A-P 3","sqrt(3249)/17 = ? don't calculate just tap 3",
+                "Answer of (371/23-12)is?  and tap 3 for next","TAP 3 if You want to go to next level",
+                "TAP 3 if You want to increase score"};
+
+        String[] Pre={"Now ","If You are alive ","If You are living ","Quick! ","Ready, ","Simon Says, ","Ahana Says, ","You must "};
 
         int ch;
         String qesn;
@@ -142,6 +172,10 @@ public class GameActivity extends AppCompatActivity {
                 qesn = Q3[(int) (Math.random() * Q3.length)];
                 break;
         }
+        if( ((int)(Math.random() * 10)) <=3 ){//40% chance
+            qesn=Pre[(int) (Math.random() * Pre.length)] + qesn;
+        }
+
         qtext.setText(qesn);
     }
 
