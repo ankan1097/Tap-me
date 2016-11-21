037d998bd400a98d6ce461656728cbf6ddff8686
Srijit Dutta <srijit04@yahoo.co.uk>
2016-11-22 01:26:23 +0530

    math xml created

---
+++ app/src/main/java/com/example/ankan/thinkb4utap/MathGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.ankan.thinkb4utap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MathGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_game);
+    }
+}
