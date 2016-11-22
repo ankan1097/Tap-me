6a52af92a1b688eb77a3ed855f39b7bf0772e783
Ankan Sardar <iankan@cse.iitb.ac.in>
2016-11-22 05:32:10 +0530

    more sound effects added

--- app/src/main/java/com/example/ankan/thinkb4utap/GameActivity.java
+++ app/src/main/java/com/example/ankan/thinkb4utap/GameActivity.java
@@ -44,6 +44,10 @@ public class GameActivity extends AppCompatActivity {
         scr = (TextView) findViewById(R.id.score);
         scr.setText(""+score);
 
+        MediaPlayer media = MediaPlayer.create(getApplicationContext(), R.raw.timer);
+        media.setLooping(true);
+        media.start();
+
         mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
         mProgressBar.setProgress(i);
         mCountDownTimer = new MyCountDownTimer(50000, Math.max(10,40-score));
@@ -61,6 +65,8 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (Integer.parseInt(bt1.getText().toString()) == ans) {
+                    MediaPlayer mp3 = MediaPlayer.create(getApplicationContext(), R.raw.correct);
+                    mp3.start();
                     ans=(int)(Math.random()*4);
                     getquestion(ans);
                     if( ((int)(Math.random() * 10)) <=4 ) {//50% chance
@@ -99,7 +105,8 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (Integer.parseInt(bt2.getText().toString()) == ans) {
-
+                    MediaPlayer mp3 = MediaPlayer.create(getApplicationContext(), R.raw.correct);
+                    mp3.start();
                     ans = (int) (Math.random() * 4);
                     getquestion(ans);
                     if( ((int)(Math.random() * 10)) <=4 ) {//50% chance
@@ -140,6 +147,8 @@ public class GameActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 if ( Integer.parseInt(bt3.getText().toString()) == ans ){
+                    MediaPlayer mp3 = MediaPlayer.create(getApplicationContext(), R.raw.correct);
+                    mp3.start();
                     ans=(int)(Math.random()*4);
                     getquestion(ans);
                     if( ((int)(Math.random() * 10)) <=4 ) {//50% chance
